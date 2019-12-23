a766163a704b8d5b363992863c2c8c441ee18da2
narenderkumarrajput <narender.nanu@gmail.com>
2019-12-23 14:52:35 +0530

    Initial setup

---
+++ InfosysMVVM/InfosysMVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InfosysMVVM
+//
+//  Created by Narender Kumar on 12/23/19.
+//  Copyright © 2019 Narender Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
